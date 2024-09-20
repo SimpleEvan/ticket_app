049aa63ba8ec7c68134100a739c75d30b600edc4
Evan <evan-dev@developabetterfuture.com>
2024-09-20 20:55:46 +0200

    created navBarBottom

---
+++ lib/core/nav_bottom_bar.dart
@@ -0,0 +1,61 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+
+class NavBottomBar extends StatefulWidget {
+  const NavBottomBar({super.key});
+
+  @override
+  State<NavBottomBar> createState() => _NavBottomBarState();
+}
+
+class _NavBottomBarState extends State<NavBottomBar> {
+  final navItems = [
+    const Center(child: Text("Home")),
+    const Center(child: Text("Search")),
+    const Center(child: Text("Tickets")),
+    const Center(child: Text("Account")),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onTappedNav(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My tickets"),
+      ),
+      body: navItems[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onTappedNav,
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: Colors.blueGrey,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+                label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+                label: "Tickets"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+                label: "Account"),
+          ]),
+    );
+  }
+}
